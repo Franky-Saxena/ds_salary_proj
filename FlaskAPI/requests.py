f799f5df32e59af27acf5cb143726e66dae8d502
Franky Saxena <86353897+Franky-Saxena@users.noreply.github.com>
2023-05-25 12:12:02 +0530

    Add files via upload

---
+++ FlaskAPI/requests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 12:00:30 2023
+
+@author: frank
+"""
+
+import requests
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL, headers=headers, json=data)
+
+r.json()
